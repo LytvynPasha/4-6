dfcc2e0ac075592904a866c945c26840c5d492fe
blck f0x <lytvyn.pavlo@gmail.com>
2021-11-02 09:58:12 +0200

    Добавьте файлы проекта.

---
+++ 4,6.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double P, S;
+    int i, k;
+    S = 0;
+    i = 1;
+    while (i <= 10)
+    {
+        P = 1;
+        k = 1;
+
+        while (k <= i)
+        {
+            P *= k / i;
+            k++;
+        }
+        S += (1 + P) / pow(i, 2);
+        i++;
+    }
+    cout << S << endl;
+    S = 0;
+    i = 1;
+    do
+    {
+        P = 1;
+        k = 1;
+        do
+        {
+
+            P *= k / i;
+            k++;
+        } while (k <= i);
+        S += (1 + P) / pow(i, 2);
+        i++;
+    } while (i <= 10);
+    cout << S << endl;
+    S = 0;
+    for (i = 1; i <= 10; i++)
+    {
+        P = 1;
+        for (k = 1; k <= i; k++)
+        {
+            P *= k / i;
+        }
+        S += (1 + P) / pow(i, 2);
+    }
+    cout << S << endl;
+    S = 0;
+    for (i = 10; i >= 1; i--)
+
+    {
+        P = 1;
+        for (k = i; k >= 1; k--)
+
+        {
+            P *= k / i;
+
+        }
+        S += (1 + P) / pow(i, 2);
+    }
+    cout << S << endl;
+    return 0;
+}
